cdbcaacb1b1c5732a739e51d0b3f0a58c5bc86c2
Christopher Browne <cbbrowne@ca.afilias.info>
2011-02-18 15:53:59 -0500

    More activity monitoring in remote_worker threads

--- src/slon/remote_worker.c
+++ src/slon/remote_worker.c
@@ -375,6 +375,8 @@ remoteWorkerThread_main(void *cdata)
 		slon_retry();
 	local_dbconn = local_conn->dbconn;
 
+	monitor_state(conn_symname, getpid(), node->no_id, local_conn->conn_pid, "just running", 0, "n/a");
+
 	/*
 	 * Put the connection into replication mode
 	 */
@@ -388,7 +390,7 @@ remoteWorkerThread_main(void *cdata)
 	 * Work until shutdown or node destruction
 	 */
 	while (true)
-	{
+	  {
 		/*
 		 * If we got the special WMSG_WAKEUP, check the current runmode of the
 		 * scheduler and the status of our node.
@@ -1556,7 +1558,7 @@ remoteWorkerThread_main(void *cdata)
 				(void) slon_mkquery(&query1, "rollback transaction;");
 				archive_terminate(node);
 			}
-			monitor_state(conn_symname, getpid(), node->no_id, local_conn->conn_pid, "just running", 0, "n/a");
+			monitor_state(conn_symname, getpid(), node->no_id, local_conn->conn_pid, "just running", event->ev_seqno, event->ev_type);
 			if (query_execute(node, local_dbconn, &query1) < 0)
 				slon_retry();
 
