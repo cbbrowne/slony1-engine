0d6e0d8c6c6f390701b01d13759ab12683aef1f2
Christopher Browne <cbbrowne@ca.afilias.info>
2011-05-03 11:19:52 -0400

    Add fodder for bug #137 in DDL test

--- tests/testddl/exec_ddl.sh
+++ tests/testddl/exec_ddl.sh
@@ -3,14 +3,16 @@ echo "
   EXECUTE SCRIPT (
        SET ID = 1,
        FILENAME = '${testname}/ddl_updates.sql',
-       EVENT NODE = 1
+       EVENT NODE = 1,
+       lock='public.table4,public.billing_discount'
     );
 
   try {
        execute script (
             set id=1,
             filename='${testname}/bad_ddl.sql',
-            event node=1
+            event node=1,
+            lock='public.snobol'
        );
   } on error {
      echo 'a bad DDL script gets rolled back on the master';
