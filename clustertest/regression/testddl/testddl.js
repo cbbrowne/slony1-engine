e6eb7808f6cc3124385c974c235bb48e925af4f7
Christopher Browne <cbbrowne@ca.afilias.info>
2011-11-15 13:45:38 -0500

    Add quoting of node numbers so EXECUTE ONLY ON is compatible with new handling of DDL

--- clustertest/regression/testddl/testddl.js
+++ clustertest/regression/testddl/testddl.js
@@ -96,7 +96,7 @@ function individual_ddl(coordinator, nodenum) {
 	
 	premable = get_slonik_preamble();
 	slonikScript = 'EXECUTE SCRIPT(set id=1, FILENAME=\'regression/testddl/ddl_update_part2.sql\''
-		+ ' ,EVENT NODE=' + nodenum + ' ,EXECUTE ONLY ON = ' + nodenum +');';
+		+ ' ,EVENT NODE=' + nodenum + ' ,EXECUTE ONLY ON = \'' + nodenum +'\');';
 	run_slonik('update ddl',coordinator,preamble,slonikScript);
 	
 }
